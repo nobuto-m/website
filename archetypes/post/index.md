b9f785dbfc7c76c01b88798e20991526118b3d2a
Nobuto Murata <nobuto@ubuntu.com>
2021-01-01 00:36:59 +0900

    slug is required when the tilte has symbols such as "/!?"

--- archetypes/post/index.md
+++ archetypes/post/index.md
@@ -2,6 +2,7 @@
 # Documentation: https://wowchemy.com/docs/managing-content/
 
 title: "{{ humanize .Name }}"
+slug: {{ .Name }}
 subtitle: ""
 summary: ""
 authors: []
--- new-post.sh
+++ new-post.sh
@@ -9,6 +9,6 @@ shopt -s expand_aliases
 printf 'New post title: '
 read -r title
 
-title_url="$(echo "$title" | tr '[:upper:]' '[:lower:]' | sed -e 's/[^a-z]/-/g' -e 's/-\+/-/g')"
+title_url="$(echo "$title" | tr '[:upper:]' '[:lower:]' | sed -e 's/[^a-z]/-/g' -e 's/-\+/-/g' -e 's/-$//')"
 year="$(date '+%Y')"
 hugo new --kind post "post/$year/${title_url}"
