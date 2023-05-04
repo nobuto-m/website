d81c39c7996ad561caa37d4d501918bcea83d596
Nobuto Murata <nobuto@ubuntu.com>
2023-05-04 22:47:35 +0900

    warn about big files

--- bin/build.sh
+++ bin/build.sh
@@ -4,6 +4,9 @@ set -e
 set -u
 set -x
 
+# print files bigger than 1 MB
+find . -path ./.git -prune -o -type f -size +1M -print
+
 # this wipes the Orientation tag too
 exiftool -P -All= -overwrite_original -r content/
 
