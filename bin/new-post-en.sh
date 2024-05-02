cf426267d920b28a1d5168dce8a2e559713674b3
Nobuto Murata <nobuto@ubuntu.com>
2024-05-02 23:57:33 +0900

    keep numbers

--- bin/new-post-en.sh
+++ bin/new-post-en.sh
@@ -6,6 +6,6 @@ set -u
 printf 'New post title: '
 read -r title
 
-title_url="$(echo "$title" | tr '[:upper:]' '[:lower:]' | sed -e 's/[^a-z]/-/g' -e 's/-\+/-/g' -e 's/-$//')"
+title_url="$(echo "$title" | tr '[:upper:]' '[:lower:]' | sed -e 's/[^a-z0-9]/-/g' -e 's/-\+/-/g' -e 's/-$//')"
 year="$(date '+%Y')"
 hugo new --kind post "post/$year/${title_url}"
